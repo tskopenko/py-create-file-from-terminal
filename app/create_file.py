ace378d0bcceaf25e45a5b43c608a86ee51d7b59
Tymofii Skopenko <youmustdeth@gmail.com>
2024-02-02 18:29:57 +0200

    update fix (now u can input flags in any sequence)

--- app/create_file.py
+++ app/create_file.py
@@ -1,10 +1,10 @@
-import sys
 import os
+import argparse
 from datetime import datetime
 
 
 def create_directory(way: str) -> None:
-    os.makedirs(way)
+    os.makedirs(way, exist_ok=True)
     current_directory = os.getcwd()
     os.chdir(os.path.join(current_directory, way))
 
@@ -16,21 +16,24 @@ def create_file(name: str) -> None:
         line = input("Enter content line: ")
         if line == "stop":
             break
-        file_content += "\n" + str(count_lines) + " " + line
+        file_content += f"\n{count_lines} {line}"
         count_lines += 1
-    with open(name, "w") as file:
+    with open(name, "a") as file:
+        if os.stat(name).st_size > 0:
+            file.write("\n")
         file.write(file_content)
 
 
 def main() -> None:
-    command = sys.argv
 
-    if "-d" in command:
-        if "-f" not in command:
-            path = os.path.join(*command[2:])
-        else:
-            path = os.path.join(*command[2:-2])
-        create_directory(path)
-    if "-f" in command:
-        file_name = command[-1]
-        create_file(file_name)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-d", dest="directory", nargs="*")
+    parser.add_argument('-f', dest="file")
+    args = parser.parse_args()
+
+    if args.directory:
+        create_directory(os.path.join(*list(args.directory)))
+    if args.file:
+        create_file(args.file)
+
